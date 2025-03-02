a346a3330cfb0aee2562375ec4847047fc072ee9
savitar696 <karosen1448@gmail.com>
2025-03-02 15:38:38 +0300

    feat: discord

--- src/features/settings/components/discord-link.tsx
+++ src/features/settings/components/discord-link.tsx
@@ -10,7 +10,7 @@ export const DiscordLink = ({ username }: { username: string }) => {
         size="xs"
         variant="ghost"
         onClick={() =>
-          (window.location.href = `https://discord.com/oauth2/authorize?client_id=1342539311181987941&response_type=code&redirect_uri=https%3A%2F%2Fapi.slowryz.tech%2Fapi%2Fdiscord%2Fauth%2Fcallback&scope=identify&state=${username}`)
+          (window.location.href = `https://discord.com/oauth2/authorize?client_id=1342539311181987941&response_type=code&redirect_uri=https%3A%2F%2Fapi.hardtournaments.space%2Fapi%2Fdiscord%2Fauth%2Fcallback&scope=identify&state=${username}`)
         }
       >
         Привязать
