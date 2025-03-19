6ae66e766c8a2ca1073393ba0611dfc18dc43062
savitar696 <karosen1448@gmail.com>
2025-03-20 02:05:28 +0300

    fix: localhost

--- src/entities/user/model/user.events.ts
+++ src/entities/user/model/user.events.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const queueIO = io("http://localhost:5000" + "/queue", {
+export const queueIO = io("https://api.hardtournaments.space" + "/queue", {
   autoConnect: false,
 });
--- src/shared/api/api.instance.ts
+++ src/shared/api/api.instance.ts
@@ -1,4 +1,4 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: "http://localhost:5000" + "/api",
+  baseURL: "https://api.hardtournaments.space" + "/api",
 });
